940e690f57279869f88e16ed13565bf057bac0c0
Timon de Groot <timon@marissen.net>
2018-03-08 00:13:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# CSV Filter (alpha)
+Written in python, this program is a command-line interface, providing the ability to mutate data in a CSV file when spreadsheet applications lack functionality.
+
+## Usage
+For now, calling the program looks a bit rough, but it gets the job done.
+
+You can call the program this way:
+`python csv_filter/__init__.py`
+
+- **--file/-f**: Specify file to filter
+- **--deduplicate/-d** (optional): Specify deduplication column
+- **--filter** (optional): Specify filter column and regex pattern
+- **--case_insensitive/-i** (optional): Enable case insensitivity
+- **--verbose/-v** (optional): Enable verbose output
+
+## Examples
+
+#### Deduplicating rows based on a column uniqueness
+`python csv_filter/__init__.py --file export.csv --deduplicate=email -i -v`
+
+#### Filter rows based on a regex
+`python csv_filter/__init__.py --file export.csv --filter "_address_country_id=(NL|BE)" -i -v`
+
+## Issue reporting/contributing
+This program is in it's very early stages. If you encounter problems or have suggestions, please create a ticket or a pull request.
---
+++ csv_filter/__init__.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+import argparse
+import csv
+import logging
+import os
+import re
+import sys
+
+DELIMITER = ','
+
+class CsvFilter():
+    def __init__(self, file=None, deduplicate=False, filter=False, case_insensitive=False, verbose=False, delimiter=DELIMITER):
+        self.file = file
+        self.deduplicate = deduplicate
+        self.filter = filter
+        self.case_insensitive = case_insensitive
+        self.verbose = verbose
+        self.delimiter = delimiter
+        self.logger = logging.getLogger('deduplicate')
+        if self.verbose:
+            self.logger.setLevel(logging.DEBUG)
+
+    def apply(self):
+        base_path = os.getcwd()
+        src_path = base_path + '/' + self.file
+        dest_path = src_path.replace('.csv', '.filtered.csv')
+        filtered_items = self.filter_items(src_path)
+        self.store_items(dest_path, filtered_items)
+
+    def filter_items(self, file_path):
+        result = []
+        deduplicate_column_index = False
+        deduplicate_key_values = []
+        filter_column = False
+        filter_column_index = False
+        filter_pattern = False
+        counter = 0
+        re_flags = 0
+
+        if self.case_insensitive:
+            re_flags = re.IGNORECASE
+
+        if self.verbose:
+            print '* Filtering file', file_path
+
+        with open(file_path, 'rb') as csvfile:
+            reader = csv.reader(csvfile, delimiter=self.delimiter)
+            for row in reader:
+                if counter == 0:
+                    if self.deduplicate:
+                        deduplicate_column_index = row.index(self.deduplicate)
+                    if self.filter:
+                        filter_match = re.match('^(.+)=(.+)$', self.filter)
+                        if filter_match:
+                            filter_column = filter_match.group(1)
+                            filter_pattern = filter_match.group(2)
+                            filter_column_index = row.index(filter_column)
+                    counter += 1
+                    result.append(row)
+                    continue
+
+                valid = True
+
+                if self.deduplicate and deduplicate_column_index is not False:
+                    value = row[deduplicate_column_index]
+                    if self.case_insensitive:
+                        value = value.lower()
+                    if value in deduplicate_key_values:
+                        valid = False
+                    else:
+                        deduplicate_key_values.append(value)
+
+                if self.filter and filter_column_index:
+                    value = row[filter_column_index]
+                    if not re.match(filter_pattern, value, re_flags):
+                        valid = False
+
+                if valid:
+                    result.append(row)
+
+                counter += 1
+
+        if self.verbose:
+            print '* Filtered', counter, 'items to', len(result)
+
+        return result
+
+    def store_items(self, file_path, items):
+        if self.verbose:
+            print '* Storing items to', file_path
+
+        with open(file_path, 'wb') as csvfile:
+            writer = csv.writer(csvfile, delimiter=self.delimiter)
+            for row in items:
+                writer.writerow(row)
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', '-f', help='File to filter')
+    parser.add_argument('--deduplicate', help='Deduplication column to be applied', default=False)
+    parser.add_argument('--filter', help='Filter to be applied', default=False)
+    parser.add_argument('--case_insensitive', '-i', action='store_true', help='Match values case insensitive', default=False)
+    parser.add_argument('--verbose', '-v', action='store_true', help='Enable verbose')
+    return parser.parse_args();
+
+def main():
+    args = parse_arguments();
+    CsvFilter(
+        file=args.file,
+        deduplicate=args.deduplicate,
+        filter=args.filter,
+        case_insensitive=args.case_insensitive,
+        verbose=args.verbose
+    ).apply()
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
